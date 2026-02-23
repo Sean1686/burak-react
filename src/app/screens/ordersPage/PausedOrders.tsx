ee4355f72e0403692d355296108e2adfbb0ecc8d
Sean1686 <khudayberdi11@gmail.com>
2026-02-24 04:46:21 +0900

    feat: publish helpPage component

---
+++ src/app/screens/ordersPage/PausedOrders.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Box, Stack } from "@mui/material";
+import Button from "@mui/material/Button";
+import TabPanel from "@mui/lab/TabPanel";
+
+export default function PausedOrders() {
+  return (
+    <TabPanel value="1">
+      <Stack>
+        {[1,2].map((ele, index) =>  {
+            return (
+              <Box className={"order-box-scroll"}>
+                {[1,2,3].map((ele2, index2) => {
+                  return (
+                    <Box key={index2} className={"orders-name-price"}>
+                      <img 
+                      src={"/img/lavash.webp"}
+                      className={"order-dish-img"} 
+                      />
+                      <p className={"title-dish"}>Lavash</p>
+                      <Box>
+                        <p>$9</p>
+                        <img src={"/icons/close.svg"} />
+                        <p>2</p>
+                        <img src="/icons/pause.svg" />
+                        <p style={{marginLeft: "15px"}}>$24</p>
+                      </Box>
+                    </Box>
+                  );
+                })}
+              </Box>
+            )
+        })}
+      </Stack>
+    </TabPanel>
+  )
+}
--- src/app/screens/ordersPage/index.tsx
+++ src/app/screens/ordersPage/index.tsx
@@ -1,5 +1,67 @@
-import { Container } from "@mui/material";
+import { useState, SyntheticEvent } from "react";
+import { Box, Container, Stack } from "@mui/material";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import PausedOrders from "./PausedOrders";
+import ProcessOrders from "./ProcessOrders";
+import FinishedOrders from "./FinishedOrders";
+import "../../../css/order.css";
 
 export default function OrdersPage() {
-  return <Container>OrdersPage</Container>;
+  const [value, setValue] = useState("1");
+
+  const handleChange = (e: SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={"order-page"}>
+      <Container className={"oder-container"}>
+        <Stack className={"order-left"}>
+          <TabContext value={value}>
+            <Box className={"order-nav-frame"}>
+              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <Tabs
+                  value={value}
+                  onChange={handleChange}
+                  aria-label="basic tabs example"
+                  className={"table_list"}
+                >
+                  <Tab label="PAUSED ORDERS" value={"1"} />
+                  <Tab label="PROCESS ORDERS" value={"2"} />
+                  <Tab label="FINISHED ORDERS" value={"3"} />
+                </Tabs>
+              </Box>
+            </Box>
+            <Stack className="order-main-content">
+              <PausedOrders />
+              <ProcessOrders />
+              <FinishedOrders />
+            </Stack>
+          </TabContext>
+        </Stack>
+
+        <Stack className="order-right">
+          <Box>
+            <Box className="user-card">
+              <img
+                src="/icons/default-user.svg"
+                className="order-user-avatar"
+              />
+
+              <h3 className="order-user-name">Justin</h3>
+              <span className="order-user-proof">USER</span>
+
+              <div className="user-location">
+                <LocationOnIcon />
+                <span>South Korea, Busan</span>
+              </div>
+            </Box>  
+          </Box>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
