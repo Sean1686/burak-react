8f6ec37fe7e0fc0aad8a5449eeab8693d6022e50
Sean1686 <khudayberdi11@gmail.com>
2026-03-26 20:29:29 +0900

    feat: modify processOrders and FinishedOrders components

--- src/app/screens/ordersPage/ProcessOrders.tsx
+++ src/app/screens/ordersPage/ProcessOrders.tsx
@@ -1,30 +1,50 @@
 import { TabPanel } from "@mui/lab";
 import { Box, Button, Stack } from "@mui/material";
+import { createSelector } from "reselect";
+import { retrieveProcessOrders } from "./selector";
+import { useSelector } from "react-redux";
+import { Order, OrderItem } from "../../../lib/types/order";
+import { Product } from "../../../lib/types/product";
+import { sereverAPI } from "../../../lib/config";
+
+
+
+
+const processOrdersRetriever = createSelector(
+  retrieveProcessOrders,
+  (processOrders) => ({ processOrders})
+);
 
 export default function ProcessOrders () {
+    const {processOrders} = useSelector(processOrdersRetriever);
+  
     return (
         <TabPanel value="2">
              <Stack>
-                    {[1, 2, ].map((ele, index) => {
+                    {processOrders?.map((order: Order) => {
                       return (
-                        <Box key={index} className={"order-main-box"}>
+                        <Box key={order._id} className={"order-main-box"}>
                           <Box className={"order-box-scroll"}>
-                            {[1, 2].map((ele2, index2) => {
+                            {order?.orderItems?.map((item: OrderItem) => {
+                  const product: Product = order.productData.filter(
+                    (ele: Product) => item.productId === ele._id
+                  )[0]
+                  const imagePath = `${sereverAPI}/${product.productImages[0]}`
                               return (
-                                <Box key={index2} className={"orders-name-price"}>
+                                <Box key={item._id} className={"orders-name-price"}>
                                     <Box className="img-name">
                                   <img
                                     src={"/img/lavash.webp"}
                                     className={"order-dish-img"}
                                   />
-                                  <p className={"title-dish"}>Lavash</p>
+                                  <p className={"title-dish"}>{product.productName}</p>
                                    </Box>
                                   <Box className={"price-box"}>
-                                    <p>$9</p>
+                                    <p>${item.itemPrice}</p>
                                     <img src={"/icons/close.svg"} />
-                                    <p>2</p>
+                                    <p>{item.itemQuantity}</p>
                                     <img src={"/icons/pause.svg"} />
-                                    <p style={{ marginLeft: "15px" }}>$24</p>
+                                    <p style={{ marginLeft: "15px" }}>${item.itemQuantity * item.itemPrice}</p>
                                   </Box>
                                 </Box>
                               );
@@ -33,23 +53,31 @@ export default function ProcessOrders () {
                           <Box className='proceed'>
                             <Box className='calc'>
                             <p className="pricing">Product price</p>
-                            <p>$60</p>
+                            <p>${order.orderTotal - order.orderDelivery}</p>
                             <img src="/icons/plus.svg" alt="" />
                             <p>Delivery cost</p>
-                            <p>$5</p>
+                            <p>${order.orderDelivery}</p>
                             <img src="/icons/pause.svg" alt="" />
                             <p>Total</p>
-                            <p>$20</p>
-                           
+                            <p>${order.orderTotal}</p>
+
                             </Box>
-            
+
                                 <p> 23-11-04 03:05 </p>
                             <Button  sx={{background:'#3A87CB', color:'white', width:'138px', height:'36px',whiteSpace:'nowrap', borderRadius:'10px'}}>VERIFY TO FULFIL</Button>
                           </Box>
                         </Box>
                       );
-                    })}
-                  </Stack>
-        </TabPanel>
-    )
+                    })} 
+                    {!processOrders || (processOrders.length === 0  && (
+            <Box display={"flex"} flexDirection={"row"} justifyContent={"center"}>
+              <img 
+              src={"/icons/noimage-list.svg"}
+              style={{ width: 300, height: 300}}
+              />
+            </Box>
+        ))}
+      </Stack>
+    </TabPanel>
+  );
 }
